ada066a22b14fc6f7c16003661ccf5d4cca94bc2
gustavo <gustavo.maia@b2b.com.pt>
2020-09-29 09:03:21 +0100

    Insert & Remove Items

--- app/src/main/java/com/artemissoftware/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/simplerecyclerview/MainActivity.kt
@@ -2,22 +2,43 @@ package com.artemissoftware.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val exampleList = generateDummyList(24)
+    private val adapter = ExampleAdapter(exampleList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recycler_view.adapter = ExampleAdapter(generateDummyList(20))
+        recycler_view.adapter = adapter
         recycler_view.layoutManager = LinearLayoutManager(this)
         recycler_view.setHasFixedSize(true)
     }
 
 
-    private fun generateDummyList(size: Int): List<ExampleItem> {
+    fun insertItem(view: View){
+
+        val index = Random.nextInt(8)
+        val newItem = ExampleItem(R.drawable.ic_android, "New item at position $index", "Line 2")
+        exampleList.add(index, newItem)
+        adapter.notifyItemInserted(index)
+    }
+
+    fun removeItem(view: View){
+        val index = Random.nextInt(8)
+        exampleList.removeAt(index)
+        adapter.notifyItemRemoved(index)
+    }
+
+
+    private fun generateDummyList(size: Int): ArrayList<ExampleItem> {
         val list = ArrayList<ExampleItem>()
         for (i in 0 until size) {
             val drawable = when (i % 3) {
