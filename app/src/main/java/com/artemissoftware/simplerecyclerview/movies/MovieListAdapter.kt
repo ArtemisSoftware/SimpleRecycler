291362b653dc92393034a7be4453c22c8796a166
gustavo <gustavo.maia@b2b.com.pt>
2020-12-04 14:58:40 +0000

    MovieListAdapter

---
+++ app/src/main/java/com/artemissoftware/simplerecyclerview/movies/MovieListAdapter.kt
@@ -0,0 +1,43 @@
+package com.artemissoftware.simplerecyclerview.movies
+
+import android.R
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.artemissoftware.simplerecyclerview.databinding.ItemMovieBinding
+import kotlinx.android.synthetic.main.item_movie.view.*
+
+
+class MovieListAdapter : ListAdapter<Movie, MovieListAdapter.MovieViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val binding = ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.bind(currentItem)
+    }
+
+    class MovieViewHolder(private val binding: ItemMovieBinding) : ViewHolder(binding.root) {
+
+
+        fun bind(movie: Movie) {
+
+            itemView.apply {
+                nameTextview.text = movie.name
+                ratingTextView.text = movie.rating
+            }
+        }
+
+
+
+    }
+
+
+}
