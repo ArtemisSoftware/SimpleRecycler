0d17c486a1f07327a3671478a2fe3d933a2e6c6d
gustavo <gustavo.maia@b2b.com.pt>
2020-12-04 15:55:04 +0000

    MovieActivity

--- app/src/main/java/com/artemissoftware/simplerecyclerview/movies/MovieActivity.kt
+++ app/src/main/java/com/artemissoftware/simplerecyclerview/movies/MovieActivity.kt
@@ -1,12 +1,30 @@
 package com.artemissoftware.simplerecyclerview.movies
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.artemissoftware.simplerecyclerview.R
+import kotlinx.android.synthetic.main.activity_movie.*
 
 class MovieActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
+
+        val movieListAdapter = MovieListAdapter()
+        //recyclerView.adapter = movieListAdapter
+
+        rcl_movies.apply {
+            adapter = movieListAdapter
+            layoutManager = LinearLayoutManager(this@MovieActivity)
+            setHasFixedSize(true)
+        }
+
+
+        val movieList = listOf<Movie>(Movie("Captain America", "8"),Movie("Iron Man", "7"), Movie("Thor", "6") )
+        movieListAdapter.submitList(movieList)
     }
 }
