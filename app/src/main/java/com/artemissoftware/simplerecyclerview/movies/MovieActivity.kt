9d1bbe599441d05efdfa8094506b5db47ace23fa
gustavo <gustavo.maia@b2b.com.pt>
2020-12-04 15:04:37 +0000

    Layout activity_movie

---
+++ app/src/main/java/com/artemissoftware/simplerecyclerview/movies/MovieActivity.kt
@@ -0,0 +1,12 @@
+package com.artemissoftware.simplerecyclerview.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.artemissoftware.simplerecyclerview.R
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
